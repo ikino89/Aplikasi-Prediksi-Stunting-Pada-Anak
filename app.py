c015092820fe40ac90f84c0bce170c0e99d22186
ikino89 <151890381+ikino89@users.noreply.github.com>
2024-11-06 20:45:47 +0700

    upload
    tes

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import KNeighborsClassifierr
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
